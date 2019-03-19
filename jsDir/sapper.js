5c3303618a362456291bbf03ce9ef3666f5f0fb7
ICE <ice8scream@gmail.com>
2019-03-19 20:31:48 +0300

    more methods

--- jsDir/sapper.js
+++ jsDir/sapper.js
@@ -6,58 +6,36 @@ let createElem = function(elemType, elemClass) {
     elem.classList.add(elemClass);
     return elem;
 }
-let fieldRows = 12, fieldCells = 12, fieldDifficult = "recruit";
-var field = new sapperField(fieldRows,fieldCells,fieldDifficult);
+let fieldRows = 20, fieldCells = 20, fieldDifficult = "recruit";
 
 
 
-console.log(field.GetFieldMatx);
-// alert(field.FieldMatx());
-let bombMatx = field.GetFieldMatx;
 
+// console.log(field.GetFieldMatx);
+// // alert(field.FieldMatx());
+// let bombMatx = field.GetFieldMatx;
+
+//push to class
 let sapper = createElem('section', 'content');
 let sapperDiscr = createElem('div', 'content-caption');
-sapperDiscr.textContent = "Bombs quantity: " + field.GetBombsCounter();
+//sapperDiscr.textContent = "Bombs quantity: " + field.GetBombsCounter();
 sapper.appendChild(sapperDiscr);
-for(let i=0; i< fieldRows; i++){
-    let tRow = createElem('div', 'content-row');
-    for(let j=0; j< fieldCells; j++){
-        let tCell = createElem('div', 'content-cell');
-        tCell.onclick = function() {
-             field.LeftClickDetected(sapper,i,j);
-        };
-        /*if(bombMatx[i][j] == 0){
-            // tCell.classList.add('bomb-cell');
-            tCell.onclick = function() {
-                // alert('you');
-                tCell.classList.add('bomb-cell');
-            };
-        } else {
 
-        }*/
-
-        /*if(bombMatx[i][j] == -2) {
-            tCell.classList.add('bomb-cell');
-        }else if(bombMatx[i][j] > 0){
-            tCell.classList.add('bomb-near');
-            tCell.textContent = bombMatx[i][j];
-        }
-        if(i == fieldRows-1 && j == fieldCells-1){
-            tCell.onclick = function(){
-                document.body.removeChild(sapper);
-                let button = createElem('div', 'content-cell');
-                button.onclick = function() {
-                    document.body.removeChild(button);
-                    document.body.appendChild(sapper);
-                };
-                document.body.appendChild(button);
-            };
-        }*/
-        tRow.appendChild(tCell);
-    }    
-    sapper.appendChild(tRow);
-}
-document.body.appendChild(sapper);
+var field = new sapperField(sapper, fieldRows,fieldCells,fieldDifficult);
+//push to class
+// for(let i=0; i< fieldRows; i++){
+//     let tRow = createElem('div', 'content-row');
+//     for(let j=0; j< fieldCells; j++){
+//         let tCell = createElem('div', 'content-cell');
+//         tCell.onclick = function() {
+//              field.LeftClickDetected(sapper,i,j);
+//         };
+       
+//         tRow.appendChild(tCell);
+//     }    
+//     sapper.appendChild(tRow);
+// }
+// document.body.appendChild(sapper);
 
 //TODO norm algorytm
 // TODO onclick widesearch and detect bombs and disable onckick
--- jsDir/sapperField.js
+++ jsDir/sapperField.js
@@ -1,8 +1,75 @@
 class sapperField {  
-    constructor(rows, cells, difficult) {
-        this.Reset(rows, cells);
-        this.FillField(this.diffInterp[difficult]);
+    constructor(node, rows, cells, difficult) {
+        this.SetFieldParams(node, rows, cells, difficult);
+        this.CreateFieldDocument(this.node);
+        this.GameStart();
     }; 
+
+    ClearField(node) {
+        let rows = this.fieldParams.rows;
+        let cells = this.fieldParams.cells;
+        
+        for(let i = 0; i < rows; i++){
+            for(let j = 0; j < cells; j++){
+                this.CellRemove(this.GetFieldCell(node,i,j));
+            }
+        }
+    };
+
+    CellRemove(node) {
+        node.classList.remove('bomb-cell',  'safe-cell', 'bomb-near', 'bomb-explode', 'gray-cell');
+        node.textContent = null;
+    };
+
+    SetFieldParams(node, rows, cells, difficult) {
+        this.node = node;
+        this.fieldParams.rows = rows;
+        this.fieldParams.cells = cells;
+        this.fieldParams.difficult = difficult;
+    };
+
+    GameStart() {
+        let fp = this.fieldParams;
+        this.Reset(fp.rows, fp.cells);
+        this.FillField(this.diffInterp[fp.difficult]);
+        this.UpdateFieldCellClick(this.node);
+        this.SetCaptionText(this.node, this.GetBombsCounter());
+    };
+
+
+
+    CreateFieldDocument(node){
+        let fp = this.fieldParams
+        for(let i=0; i< fp.rows; i++){
+            let tRow = createElem('div', 'content-row');
+            for(let j=0; j< fp.cells; j++){
+                let tCell = createElem('div', 'content-cell');
+                tRow.appendChild(tCell);
+            }    
+            node.appendChild(tRow);
+        }
+        document.body.appendChild(node);
+    };
+
+    
+
+    UpdateFieldCellClick(node){
+        let fp = this.fieldParams
+        for(let i=0; i< fp.rows; i++){
+            for(let j=0; j< fp.cells; j++){
+                let tCell = this.GetFieldCell(node, i, j);
+                let cFunc = function() {
+                    this.LeftClickDetected(this.node,i,j);
+                };
+                tCell.onclick = cFunc.bind(this);
+            }
+        }
+    };
+
+    SetCaptionText(node, message) {
+        let cap = node.querySelector('.content-caption');
+        cap.textContent = message;
+    };
 
     //reset field
     Reset(rows, cells) {
@@ -14,6 +81,7 @@ class sapperField {
             }
             this.field.push(fieldRow);
         }
+        this.bombCounter = 0;
     };
 
     //fill field with bombs
@@ -68,8 +136,8 @@ class sapperField {
 
     // OnClickListener for LKM (TODO check it) 
     LeftClickDetected(node,i,j){
-        if(this.field[i][j] == -2){
-            this.GameOwer(this.GetFieldCell(node,i,j));
+        if(this.field[i][j] === -2){
+            this.GameOwer(node,i,j);
         }else if(this.field[i][j]>=0){
             this.SafeFieldExpansion(node,i,j);
         }
@@ -81,9 +149,9 @@ class sapperField {
         if(this.field[a][b] > 0){
             this.ShowNearbombCell(fieldCell,a,b);
         }else{
-             if(this.field[a][b] == 0){
+             if(this.field[a][b] === 0){
                 this.field[a][b] = -3;
-                fieldCell.classList.add('test');
+                fieldCell.classList.add('safe-cell');
                 let localField = this.GetLocalField(a,b);
                 
                 for( let i = localField.i; i <= localField.bottom; i++){
@@ -99,21 +167,16 @@ class sapperField {
 
     //return element in a,b position
     GetFieldCell(node,i,j){
-        return node.querySelectorAll(".content-row")[i].querySelectorAll(".content-cell")[j];
+        return node.querySelectorAll('.content-row')[i].querySelectorAll('.content-cell')[j];
     };
 
     //show near bomb cell and stop
     ShowNearbombCell(node,a,b){
         node.classList.add('bomb-near');
         node.textContent = this.field[a][b];
+             node.style.color = this.nearColor[this.field[a][b]];
         this.field[a][b] = -3;
     };
-
-    GameOwer(node){
-        node.classList.add('bomb-cell');
-        alert("GAME OVER");
-    };
-
     // return field (0 - safety, 1-8 - nearbomb, -2 - bomb, -3 - checked)
     get GetFieldMatx(){
         return this.field;
@@ -124,12 +187,46 @@ class sapperField {
         return this.bombCounter;
     };
 
+    GameOwer(node, a, b){
+        for(let i = 0; i < this.field.length; i++){
+            for(let j = 0; j < this.field[0].length; j++){
+                if(this.field[i][j] === -2){
+                    if(i != a || j != b){
+                        this.GetFieldCell(node,i,j).classList.add('bomb-cell');   
+                    }
+                }
+
+                if (this.field[i][j] >= 0) {
+                    this.GetFieldCell(node,i,j).classList.add('gray-cell');   
+                }
+                
+                this.GetFieldCell(node,a,b).classList.add('bomb-explode');   
+                this.GetFieldCell(node,i,j).onclick = null;
+            }
+        }
+        let cap = node.querySelector('.content-caption');
+        cap.textContent = 'Retry';
+        /*let tempF = function() {
+            console.log(this);
+        };
+        cap.addEventListener('click',tempF);*/
+        let tempF = function(){
+            this.ClearField(node);
+            this.GameStart();
+            cap.onclick = null;
+        };
+
+        cap.onclick = tempF.bind(this);
+    //    GameOwer
+        alert('GAME OVER');
+    };
+
     // TODO oncClickListener for RBM and LBM
         //TODO Win/loose game logic
         // TODO for RBM make counter sincronise, if allB == allF -> wingame
-    
 
-    /*====================PROPERTIES=====================*/    
+    /*====================PROPERTIES=====================*/
+    node = null;   
     field = [];
     bombCounter = 0;
     diffInterp = {
@@ -137,4 +234,12 @@ class sapperField {
         veteran: 10,
         expert: 20
     };
+
+    fieldParams = {
+        rows: 1,
+        cols: 1,
+        difficult: "recruit"
+    }
+
+    nearColor = [null, 'blue', 'green', 'red', '#0000ff', 'brown', 'cayan', 'crimson', 'crimson'];
 };
